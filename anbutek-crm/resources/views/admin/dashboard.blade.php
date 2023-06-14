a28f309d57c05f9732c4586e3f6ddd787bc8a67e
Sajid Mansoor <cb009555@students.apiit.lk>
2023-06-14 07:30:31 +0530

    Dashboard View Change

--- anbutek-crm/resources/views/admin/dashboard.blade.php
+++ anbutek-crm/resources/views/admin/dashboard.blade.php
@@ -1,110 +1,91 @@
 <x-app-layout>
 
-    <center>
-        <h1>Welcome to the Admin Dashboard 🍾</h1>
-    </center>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Admin Dashboard 🍻') }}
+            </h2>
+        </x-slot>
 
+        <div class="container mx-auto px-4 pt-10">
+            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                <!-- Total Revenue -->
+                <div class="bg-white border rounded shadow p-4">
+                    <div class="flex items-center">
+                        <div class="rounded p-3 bg-green-600 text-white">
+                            <i class="fas fa-wallet fa-2x fa-fw"></i>
+                        </div>
+                        <div class="ml-4">
+                            <p class="text-gray-500">Total Revenue</p>
+                            <p class="font-bold text-2xl">$4,800</p>
+                        </div>
+                    </div>
+                </div>
 
-    <!--Container-->
-    <div class="container w-full mx-auto pt-20">
+                <!-- Total Users -->
+                <div class="bg-white border rounded shadow p-4">
+                    <div class="flex items-center">
+                        <div class="rounded p-3 bg-pink-600 text-white">
+                            <i class="fas fa-users fa-2x fa-fw"></i>
+                        </div>
+                        <div class="ml-4">
+                            <p class="text-gray-500">Total Users</p>
+                            <p class="font-bold text-2xl">{{ $customerCount }}</p>
+                        </div>
+                    </div>
+                </div>
 
-        <div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
+                <!-- New Users -->
+                <div class="bg-white border rounded shadow p-4">
+                    <div class="flex items-center">
+                        <div class="rounded p-3 bg-yellow-600 text-white">
+                            <i class="fas fa-user-plus fa-2x fa-fw"></i>
+                        </div>
+                        <div class="ml-4">
+                            <p class="text-gray-500">New Users</p>
+                            <p class="font-bold text-2xl">{{ $newCustomerCount }}</p>
+                        </div>
+                    </div>
+                </div>
 
-            <!--Console Content-->
+                <!-- Total Products -->
+                <div class="bg-white border rounded shadow p-4">
+                    <div class="flex items-center">
+                        <div class="rounded p-3 bg-indigo-600 text-white">
+                            <i class="fas fa-tasks fa-2x fa-fw"></i>
+                        </div>
+                        <div class="ml-4">
+                            <p class="text-gray-500">Total Products</p>
+                            <p class="font-bold text-2xl">{{ $productCount }}</p>
+                        </div>
+                    </div>
+                </div>
 
-            <div class="flex flex-wrap">
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-green-600"><i class="fa fa-wallet fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-500">Total Revenue</h5>
-                                <h3 class="font-bold text-3xl">$4,800 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
-                            </div>
+                <!-- Total Orders -->
+                <div class="bg-white border rounded shadow p-4">
+                    <div class="flex items-center">
+                        <div class="rounded p-3 bg-blue-600 text-white">
+                            <i class="fas fa-shopping-cart fa-2x fa-fw"></i>
+                        </div>
+                        <div class="ml-4">
+                            <p class="text-gray-500">Total Orders</p>
+                            <p class="font-bold text-2xl">23</p>
                         </div>
                     </div>
-                    <!--/Metric Card-->
                 </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-pink-600"><i class="fas fa-users fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-500">Total Users</h5>
-                                <h3 class="font-bold text-3xl">{{ $customerCount }} <span class="text-pink-500"><i class="fas fa-exchange-alt"></i></span></h3>
-                            </div>
+
+                <!-- Total Issues -->
+                <div class="bg-white border rounded shadow p-4">
+                    <div class="flex items-center">
+                        <div class="rounded p-3 bg-red-600 text-white">
+                            <i class="fas fa-exclamation-circle fa-2x fa-fw"></i>
+                        </div>
+                        <div class="ml-4">
+                            <p class="text-gray-500">Total Issues</p>
+                            <p class="font-bold text-2xl">3</p>
                         </div>
                     </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-yellow-600"><i class="fas fa-user-plus fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-500">New Users</h5>
-                                <h3 class="font-bold text-3xl">{{ $newCustomerCount }} <span class="text-yellow-600"><i class="fas fa-caret-up"></i></span></h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-blue-600"><i class="fas fa-server fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-500">Total Products</h5>
-                                <h3 class="font-bold text-3xl">{{ $productCount }}</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-indigo-600"><i class="fas fa-tasks fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-500">Total Orders</h5>
-                                <h3 class="font-bold text-3xl">23 <span class="text-indigo-600"><i class="fas fa-caret-up"></i></span></h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-white border rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-red-600"><i class="fas fa-inbox fa-2x fa-fw fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-500">Issues</h5>
-                                <h3 class="font-bold text-3xl">3 <span class="text-red-500"><i class="fas fa-caret-up"></i></span></h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
                 </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    </x-app-layout>
